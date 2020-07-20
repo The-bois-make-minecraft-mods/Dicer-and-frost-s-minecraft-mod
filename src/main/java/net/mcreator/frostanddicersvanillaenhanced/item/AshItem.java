0c6e337b8450c4b9b9b4cbd869ca25ea2ba86832
Dicer-Degub <Guchi Smartoilet@192.168.1.7>
2020-07-20 03:20:29 +0100

    bone tools

--- src/main/java/net/mcreator/frostanddicersvanillaenhanced/item/AshItem.java
+++ src/main/java/net/mcreator/frostanddicersvanillaenhanced/item/AshItem.java
@@ -1,12 +1,19 @@
 
 package net.mcreator.frostanddicersvanillaenhanced.item;
 
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.block.BlockState;
+
+import net.mcreator.frostanddicersvanillaenhanced.FrostAndDicersVanillaenhancedModElements;
+
 @FrostAndDicersVanillaenhancedModElements.ModElement.Tag
 public class AshItem extends FrostAndDicersVanillaenhancedModElements.ModElement {
-
 	@ObjectHolder("frost_and_dicers_vanillaenhanced:ash")
 	public static final Item block = null;
-
 	public AshItem(FrostAndDicersVanillaenhancedModElements instance) {
 		super(instance, 7);
 	}
@@ -15,9 +22,7 @@ public class AshItem extends FrostAndDicersVanillaenhancedModElements.ModElement
 	public void initElements() {
 		elements.items.add(() -> new ItemCustom());
 	}
-
 	public static class ItemCustom extends Item {
-
 		public ItemCustom() {
 			super(new Item.Properties().group(ItemGroup.MISC).maxStackSize(64));
 			setRegistryName("ash");
@@ -37,7 +42,5 @@ public class AshItem extends FrostAndDicersVanillaenhancedModElements.ModElement
 		public float getDestroySpeed(ItemStack par1ItemStack, BlockState par2Block) {
 			return 1F;
 		}
-
 	}
-
 }
